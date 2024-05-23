f731f8bbf168b70409b6bd52eda6254d5a85a186
VrajShah3010 <133755088+VrajShah3010@users.noreply.github.com>
2024-05-23 14:52:57 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://mern-project-1-gm4j.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
